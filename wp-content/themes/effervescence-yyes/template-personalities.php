756396cd50336e241193a13fc6f23a1feefd53c1
jacob wick <jacob.wick@gmail.com>
2019-04-05 14:09:33 -0600

    sommeliers & winemakers

--- wp-content/themes/effervescence-yyes/template-personalities.php
+++ wp-content/themes/effervescence-yyes/template-personalities.php
@@ -98,7 +98,7 @@
 
         
       <?php if ($participants_bottom) : ?>
-         <h2 class="h3 mt-6 mb-4 b-rule-text" id="<?php echo $anchor_bottom ?>"><?php _e('Sommeliers'); ?></h2>
+         <h2 class="h3 mt-6 mb-4 b-rule-text" id="<?php echo $anchor_bottom ?>"><?php _e('Sommeliers & Winemakers'); ?></h2>
          <ul class="participant-grid mb-4">
             <?php foreach($participants_bottom as $participant) : ?>
 
